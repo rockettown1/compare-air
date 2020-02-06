ef0d9f8d56d2b0a46c23f54ba6e65d947bca5e27
Dan Krishnan <dan@wearecodenation.com>
2020-02-06 03:43:23 +0000

    Updates

---
+++ precache-manifest.5e5ef6dab1ee05d201e86eda15430936.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "146c578203099f19eec3646652c25875",
+    "url": "/compare-air/index.html"
+  },
+  {
+    "revision": "7fe25365f01d122f48e2",
+    "url": "/compare-air/static/css/main.daa0c805.chunk.css"
+  },
+  {
+    "revision": "58dfaaa2f9d1684f3ebc",
+    "url": "/compare-air/static/js/2.ed323aca.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/compare-air/static/js/2.ed323aca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fe25365f01d122f48e2",
+    "url": "/compare-air/static/js/main.deee5c86.chunk.js"
+  },
+  {
+    "revision": "bec77fa5e3d7ccb0c250",
+    "url": "/compare-air/static/js/runtime-main.0f6fc234.js"
+  },
+  {
+    "revision": "ce5bbb854f0fa80d52393cd6b68057ea",
+    "url": "/compare-air/static/media/close.ce5bbb85.svg"
+  },
+  {
+    "revision": "8617f4faf4a1e21776b344a0ca68a485",
+    "url": "/compare-air/static/media/magnifying-glass.8617f4fa.svg"
+  }
+]);
